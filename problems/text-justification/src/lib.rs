e1865a6a5aed3f47857c2d751938a3e2c9d1e156
import-benjamin <import-benjamin@noreply.codeberg.org>
2025-11-11 21:51:54 +0100

    tests(text-justification): add unit tests

--- problems/text-justification/src/lib.rs
+++ problems/text-justification/src/lib.rs
@@ -1,5 +1,5 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+fn full_justify(words: Vec<String>, max_width: i32) -> Vec<String> {
+    words
 }
 
 #[cfg(test)]
@@ -7,8 +7,26 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn case_1() {
+        let input: Vec<String> = ["This", "is", "an", "example", "of", "text", "justification."].iter().map(|s| s.to_string()).collect();
+        let expected: Vec<String> = ["This    is    an", "example  of text", "justification.  "].iter().map(|s| s.to_string()).collect();
+        let result = full_justify(input, 16);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn case_2() {
+        let input: Vec<String> = ["What","must","be","acknowledgment","shall","be"].iter().map(|s| s.to_string()).collect();
+        let expected: Vec<String> = ["What   must   be", "acknowledgment  ", "shall be        "].iter().map(|s| s.to_string()).collect();
+        let result = full_justify(input, 16);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn case_3() {
+        let input: Vec<String> = ["Science","is","what","we","understand","well","enough","to","explain","to","a","computer.","Art","is","everything","else","we","do"].iter().map(|s| s.to_string()).collect();
+        let expected: Vec<String> = [  "Science  is  what we", "understand      well", "enough to explain to", "a  computer.  Art is", "everything  else  we", "do                  "].iter().map(|s| s.to_string()).collect();
+        let result = full_justify(input, 20);
+        assert_eq!(result, expected);
     }
 }
